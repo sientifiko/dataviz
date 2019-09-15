8d0542e8166194d45ac5cf7b0c38a37fa228e534
el-racional <esteban.carreno.c@gmail.com>
2019-09-15 18:04:28 -0300

    last commit

--- script_graficas.R
+++ script_graficas.R
@@ -1,7 +1,9 @@
 
 # lapply(c("ggplot2", "tidyverse", "ggthemes"), install.packages)
 
-library(ggplot2); library(tidyverse); library(ggthemes)
+library(ggplot2); library(tidyverse); library(ggthemes); library(scales)
+
+options(scipen = 999, digits = 2)
 
 # IMPORTAR DATA
 casen <- read.delim("casen2017_set.csv", sep = ";")
@@ -89,21 +91,105 @@ ggplot(quint_by_regs, aes(regs, perc, fill=qaut)) +
   labs(x="", y="frecuencia", fill= "Quintil ingreso\nautonomo")
 
 
-
 # ============= GRAFICAR DOS VARIABLES CUANTITATIVAS =========
 
+# graficar escolaridad e ingresos
+ggplot(casen_mayores, aes(esc, log(ingreso) )) +
+  geom_point() +
+  theme_bw() +
+  labs(x="Escolaridad en años", y="Ingresos (escala logaritmica)")
 
+# ponerle más estilo y detalles
+ggplot(na.omit(casen_mayores), aes(esc, log(ingreso))) +
+  geom_point(aes(size=numper, color=sexo), alpha = 1/3) +
+  theme_bw() +
+  geom_smooth(method = "lm") +
+  labs(x="Escolaridad en años", y="Ingresos (escala logaritmica)",
+       size="Tamaño de familia", color="Sexo")
+
+# incluir no linealidad
+ggplot(na.omit(casen_mayores), aes(esc, log(ingreso))) +
+  geom_point(aes(size=numper, color=sexo), alpha = 1/3) +
+  theme_bw() +
+  geom_smooth(method = "gam") +
+  labs(x="Escolaridad en años", y="Ingresos (escala logaritmica)",
+       size="Tamaño de familia", color="Sexo")
+  
 
 # ======= GRAFICAR UNA VARIABLE CATEGÓRICA Y UNA CUANTITATIVA======
+
+# Cajón y bigotes
+ggplot(casen_mayores, aes(sexo, esc)) +
+  geom_boxplot() +
+  theme_bw() +
+  labs(x="", y="Escolaridad en años")
+
+# Cajón y bigotes, con más detalles
+ggplot(casen_mayores, aes(sexo, esc)) +
+  geom_boxplot() +
+  theme_bw() +
+  scale_y_continuous(breaks = c(1:22)) +
+  labs(x="", y="Escolaridad en años")
+
+# plotear ingresos
+ggplot(casen_mayores, aes(regs, log(ingreso))) +
+  geom_boxplot() +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_y_continuous(breaks = seq(0, max(log(casen_mayores$ingreso), 
+                                         na.rm = T),1)) +
+  labs(x="", y="Ingresos (escala logaritmica)")
+
+
+# agregando variable sexo
+ggplot(na.omit(casen_mayores), aes(regs, log(ingreso))) +
+  geom_boxplot(outlier.alpha = 0, aes(color=sexo)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_y_continuous(breaks = seq(0, max(log(casen_mayores$ingreso), 
+                                         na.rm = T),1)) +
+  labs(x="", y="Ingresos (escala logaritmica)")
 
 
 
 # ================== GRAFICAR UNA INTERACCIÓN ===========
 
-
+# Interacción ruralidad y sexo en educación
+ggplot(casen_mayores, aes(zona, esc, color=sexo, group=sexo)) +
+  geom_point(stat = "summary", fun.y=mean) +
+  stat_summary(fun.y = mean, geom = "line") +
+  stat_summary(fun.data = mean_se, geom = "pointrange") +
+  theme_bw() +
+  theme(legend.position = "top") +
+  labs(x="", y="Media de escolaridad")
 
 
 # ================= OTRAS GRAFICAS CHIDAS ===============
+
+# el cleveland plot!!!
+
+# un set de datas para plotear
+reg_sex_incom <- casen_mayores %>% 
+  group_by(regs, sexo) %>%
+  summarize(mean_income = mean(ingreso, na.rm = T))
+
+# Cleveland dotplot
+ggplot(reg_sex_incom, aes(mean_income, regs)) +
+  geom_line(aes(group=regs)) +
+  geom_point(aes(color=sexo), size=3) +
+  theme_bw() + 
+  geom_vline(xintercept = mean(reg_sex_incom$mean_income)) +
+  theme(axis.text.x = element_text(angle = 90), 
+        legend.position = "top",
+        legend.text = element_text(face = "bold",
+                                   size = 15)) +
+  scale_x_continuous(breaks = seq(0, 
+                                  max(reg_sex_incom$mean_income),50000),
+                     labels = dollar_format(prefix = "$") ) +
+  labs(x="Media ingresos", y="", color="Sexo")
+
+
+
 
 
 
