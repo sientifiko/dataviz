2525d1b5933ab4e0552031cfc2012a93341080e2
sientifiko <socialscienceanime@gmail.com>
2019-12-23 10:37:26 -0300

    comparto datos de desigualdad en Chile

--- script_graficas.R
+++ script_graficas.R
@@ -3,7 +3,7 @@
 
 library(tidyverse); library(ggthemes); library(scales)
 
-options(scipen = 999, digits = 2)
+options(scipen = 999)
 
 # IMPORTAR DATA
 casen <- read.delim("casen2017_set.csv", sep = ";")
@@ -192,7 +192,7 @@ ggplot(reg_sex_incom, aes(mean_income, regs)) +
   labs(x="Media ingresos", y="", color="Sexo")
 
 
-# =============== Sobre desigualdad inter etaria en Chile ===========
+# =============== SOBRE DESIGUALDAD EN CHILE ===========
 casen_mayores$yautcor <- as.numeric(as.character(casen_mayores$yautcor))
 
 # ploteo box plot por edad como factor
@@ -227,5 +227,63 @@ ggplot(group_edad, aes(as.factor(edad), dif, fill=as.factor(edad))) +
   geom_hline(yintercept = 242879.2, colour = "red",
              size = 1) +
   labs(x="Edad", y= "Diferencia ingreso aut. corregido q_20 y q_80")
+
+
+# package inequality y foreign pa importar data de extensión .dta
+library(ineq); library(foreign)
+
+# dataset con Gini 2017
+gini_edad_2017 <- casen_mayores %>% group_by(edad) %>%
+  summarize(gini = ineq(ypch, type = "Gini"))
+
+# importo casen de 1990
+casen1990 <- read.dta("casen1990.dta")
+
+# dataset gini 1990
+gini_edad_1990 <- casen1990 %>% 
+  filter(edad >= 20) %>%
+  group_by(edad) %>%
+  summarize(gini = ineq(ypchaj, type = "Gini"))
+
+# asigno años
+gini_edad_2017$anno <- 2017
+gini_edad_1990$anno <- 1990
+
+# fusiono
+gini_edad <- rbind(gini_edad_1990, gini_edad_2017)
+gini_edad$gini <- gini_edad$gini * 100
+
+# ploteo
+ggplot(gini_edad, aes(as.factor(edad), gini, fill = as.factor(edad))) +
+  geom_bar(stat = "identity") +
+  theme_bw() +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90,
+                                   size = 12)) +
+  scale_x_discrete(breaks = seq(20,117, 5)) +
+  scale_y_continuous(limits = c(0,100)) +
+  facet_wrap(~anno) +
+  labs(x="Edad", y = "Ìndice Gini")
+  
+# importar set histórico del gini
+gini_data <- read.delim("gini_data.csv", sep = "\t")
+
+# plotear desigualdad
+ggplot(gini_data, aes(anno, gini)) +
+  geom_line() +
+  theme_bw() +
+  scale_y_continuous(limits = c(0,100)) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = "Edad", y = "Índice Gini nacional")
+
+
+
+
+
+
+
+
+
+
 
 
